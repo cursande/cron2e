d21a40063c71c508eeb0b6febeef20ec68159b0b
Alex F <adfarries@gmail.com>
2020-02-17 18:18:33 +1100

    Flesh out validation

--- validator.go
+++ validator.go
@@ -5,8 +5,53 @@ import (
 	"fmt"
 )
 
-// Will validate a given field's pre and post separator values with a given validator fn.
-func validateAllFieldVals(cf CronField, validator func(val int) (bool, error)) (valid bool, validationErr error) {
+func errorMessage(fieldName string, val int) error {
+	return errors.New(fmt.Sprintf("The %s '%d' is invalid", fieldName ,val))
+}
+
+func validateMinute(val int) (valid bool, validationErr error) {
+	if val <= -1 || val < 60 {
+		return true, nil
+	}
+
+	return false, errorMessage("minute", val)
+}
+
+func validateHour(val int) (valid bool, validationErr error) {
+	if val <= -1 || val < 24 {
+		return true, nil
+	}
+
+	return false, errorMessage("hour", val)
+}
+
+func validateDayMonth(val int) (valid bool, validationErr error) {
+	if (val > 0 && val < 32) || val == -1 {
+		return true, nil
+	}
+
+	return false, errorMessage("month-day", val)
+}
+
+func validateMonth(val int) (valid bool, validationErr error) {
+	if (val > 0 && val < 13) || val == -1 {
+		return true, nil
+	}
+
+	return false, errorMessage("month", val)
+}
+
+// Valid values can vary, depending on whether weekdays are zero-indexed. We assume the standard format
+// is 0-6 (Sunday to Saturday)
+func validateDayWeek(val int) (valid bool, validationErr error) {
+	if val >= -1 || val < 7 {
+		return true, nil
+	}
+
+	return false, errorMessage("weekday", val)
+}
+
+func validateFieldVals(cf CronField, validator func(int) (bool, error)) (valid bool, validationErr error) {
 	for i := 0; i < len(cf.fieldVals); i++ {
 		val := cf.fieldVals[i]
 
@@ -30,10 +75,29 @@ func validateAllFieldVals(cf CronField, validator func(val int) (bool, error)) (
 	return true, nil
 }
 
-func validateMinuteField(val int) (valid bool, validationErr error) {
-	if val < 0 || val > 59 {
-		return false, errors.New(fmt.Sprintf("The minute value '%d' is invalid", val))
+func Validate(cb *CronBreakdown) (isValid bool) {
+	validations := []struct {
+		field     CronField
+		validator func(int) (bool, error)
+	}{
+		{cb.minute, validateMinute},
+		{cb.hour, validateHour},
+		{cb.dayMonth, validateDayMonth},
+		{cb.month, validateMonth},
+		{cb.dayWeek, validateDayWeek},
 	}
 
-	return true, nil
+	for _, v := range validations {
+		valid, err := validateFieldVals(v.field, v.validator)
+
+		if !valid {
+			cb.validationErrs = append(cb.validationErrs, err)
+		}
+	}
+
+	if len(cb.validationErrs) > 0 {
+		return false
+	}
+
+	return true
 }
--- validator_test.go
+++ validator_test.go
@@ -7,7 +7,28 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestValidateMinuteField(t *testing.T) {
+func TestValidateMinute(t *testing.T) {
+	assert := assert.New(t)
+
+	testCases := []struct {
+		val           int
+		expectedValid bool
+		expectedErr   error
+	}{
+		{59, true, nil},
+		{81, false, errors.New("The minute '81' is invalid")},
+		{-1, true, nil},
+	}
+
+	for _, tc := range testCases {
+		valid, err := validateMinute(tc.val)
+
+		assert.Equal(tc.expectedErr, err)
+		assert.Equal(tc.expectedValid, valid)
+	}
+}
+
+func TestValidateFieldVals(t *testing.T) {
 	assert := assert.New(t)
 
 	min := CronField{
@@ -16,8 +37,47 @@ func TestValidateMinuteField(t *testing.T) {
 		sep:              '/',
 	}
 
-	valid, err := validateAllFieldVals(min, validateMinuteField)
+	valid, err := validateFieldVals(min, validateMinute)
 
 	assert.Equal(false, valid)
-	assert.Equal(errors.New("The minute value '68' is invalid"), err)
+	assert.Equal(errors.New("The minute '68' is invalid"), err)
+}
+
+func TestValidate(t *testing.T) {
+	assert := assert.New(t)
+
+	cb := &CronBreakdown{
+		minute: CronField{
+			fieldVals:        []int{5},
+			postSepFieldVals: []int{65},
+		},
+		hour: CronField{
+			fieldVals:        []int{-1},
+			postSepFieldVals: []int{6},
+		},
+		dayMonth: CronField{
+			fieldVals:        []int{-1},
+			postSepFieldVals: []int{32},
+		},
+		month: CronField{
+			fieldVals:        []int{6},
+			postSepFieldVals: []int{13},
+		},
+		dayWeek: CronField{
+			fieldVals: []int{-1},
+		},
+	}
+
+	valid := Validate(cb)
+
+	assert.Equal(false, valid)
+
+	assert.Equal(
+		[]error{
+			errors.New("The minute '65' is invalid"),
+			errors.New("The month-day '32' is invalid"),
+			errors.New("The month '13' is invalid"),
+		},
+		cb.validationErrs,
+	)
 }
